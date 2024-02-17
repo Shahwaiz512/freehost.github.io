e3bc54b8428d8dc1fb916e6565fd6277a62613bd
Shahwaiz512 <159876458+Shahwaiz512@users.noreply.github.com>
2024-02-17 07:20:29 -0800

    new change

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,29 +10,21 @@ function showForm(){
     sidebar.style.display = 'none'
 }
 
-
-// function toggleBorderShadow(selectedDiv) {
-//     // Reset border and box shadow for all divs
-//     var divs = document.querySelectorAll('.active1');
-//     divs.forEach(function(div) {
-//       div.style.borderRight = 'none';
-//       div.style.boxShadow = 'none';
-//     });
-  
-//     // Apply border and box shadow to the selected div
-//     var selectedDivIndex = selectedDiv - 1;
-//     var selectedDivElem = divs[selectedDivIndex];
-//     selectedDivElem.style.borderRight = '4px solid #ccc';
-//     selectedDivElem.style.boxShadow = '3px 3px 15px 0 hsla(0,0%,73%,.15)';
-// }  
-
-
-
-
-
-
 function closeAdvertisement() {
     var advertisementWrapper = document.getElementById('advertisement-wrapper');
     advertisementWrapper.style.display = 'none';
     document.body.classList.remove('blur'); // Remove blur effect from the rest of the webpage
+}
+
+
+
+function toggleInfo(elementId){
+    var maininfo  =  document.querySelectorAll('.professor-detail > div');
+    maininfo.forEach(function(div) {
+        div.style.display = 'none'
+    });
+    var show = document.querySelector('.' + elementId);
+    if(show){
+        show.style.display = 'block'
+    }
 }
