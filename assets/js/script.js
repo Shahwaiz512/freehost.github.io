0e6e4ec7ff6c338cd17e92a24de55d298c7cc180
Shahwaiz512 <159876458+Shahwaiz512@users.noreply.github.com>
2024-02-17 10:18:42 -0800

    'toggleImage

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ function showForm(){
     sidebar.style.display = 'none'
 }
 
-//for advertisment blur
+//for advertisment blur.....
 
 
 function closeAdvertisement() {
@@ -35,3 +35,47 @@ function toggleInfo(elementId){
         show.style.display = 'block'
     }
 }
+
+
+//for displaying the images of a course section....
+
+// function toggleImage(imageId){
+//     var images = document.querySelectorAll('.info-imge > div ');
+//     images.forEach(function(div) {
+//         div.style.display = 'none'
+//     });
+//     var showimage = document.querySelector('.' + imageId);
+//     if(showimage){
+//         showimage.style.display = 'block'
+//     }
+// }
+
+
+function toggleImage(elementId){
+    var maininfo  =  document.querySelectorAll('.info-image > div');
+    maininfo.forEach(function(div) {
+        div.style.display = 'none'
+    });
+    var show = document.querySelector('.' + elementId);
+    if(show){
+        show.style.display = 'block'
+    }
+}
+
+
+
+
+
+
+
+// function toggleImage(imageId) {
+//     var images = document.querySelectorAll('.info-image > div');
+//     images.forEach(function(div) {
+//         div.style.display = 'none';
+//     });
+//     var showImage = document.querySelector('.' + imageId);
+//     if (showImage) {
+//         showImage.style.display = 'block';
+//     }
+// }
+
