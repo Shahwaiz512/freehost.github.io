3f9063121040c9367c8096730c1998a63c7270ac
Shahwaiz512 <159876458+Shahwaiz512@users.noreply.github.com>
2024-02-21 09:55:49 -0800

    side bar setting

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,4 +66,13 @@ function passValid(){
 
     }
 }
-// console.log(passValid);
+// smooth scrolling--------------
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
